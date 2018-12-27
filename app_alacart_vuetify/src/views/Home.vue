d0b2309590a89837ea4e561d6eecae0d2240a4a1
Cindy Conway <cindy@homegauge.com>
2018-12-27 13:00:08 -0500

    load all of vuetify

---
+++ app_alacart_vuetify/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Time Piker Test</h1>
+    <time-test/>
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld.vue';
+import TimeTest from '../../../src/components/TimeTest.vue';
+
+export default {
+  components: {
+    HelloWorld,
+    TimeTest,
+  },
+};
+</script>
---
+++ src/components/TimeTest.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <v-layout row wrap>
+    <v-flex md12 lg6>
+      <v-time-picker v-model="e4" color="green lighten-1"></v-time-picker>
+    </v-flex>
+    <v-flex md12 lg6 class="hidden-xs-only">
+      <h1>test test</h1>
+      <v-time-picker v-model="e4" color="green lighten-1" header-color="primary"></v-time-picker>
+    </v-flex>
+  </v-layout>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      e4: null,
+    };
+  },
+};
+</script>
